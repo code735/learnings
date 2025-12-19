0021afd755ad7e269266a0718407b002a9a2f943
code735 <prathmeshjkumbhar7777777@gmail.com>
2025-12-19 20:13:08 +0530

    just simple query parameters in fastapi

--- learning-fastapi/main.py
+++ learning-fastapi/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI;
 app = FastAPI()
 
+students = {
+  1: {
+    "name" : "john",
+    "age" : "17",
+    "class" : "12th"
+  }
+}
 
 @app.get("/")
 def home():
   return {"get": "wassup bitches"}
+
+@app.get("/get-student/{id}")
+def get_student(id: int):
+  return students[id]
