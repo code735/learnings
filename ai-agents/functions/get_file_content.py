0054dfdb024bf4b482afa8bf23e1632105b08b37
code735 <prathmeshjkumbhar7777777@gmail.com>
2025-12-30 18:49:09 +0530

    read files
    git

---
+++ ai-agents/functions/get_file_content.py
@@ -0,0 +1,27 @@
+import os
+
+def get_file_content(working_directory, file_path):
+    MAX_CHARS = 10000
+    try:
+        abs_working_dir = os.path.abspath(working_directory)
+        target_file = os.path.normpath(os.path.join(abs_working_dir, file_path))
+
+        if os.path.commonpath([abs_working_dir, target_file]) != abs_working_dir:
+            return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
+
+        if not os.path.isfile(target_file):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+
+        with open(target_file, "r", encoding="utf-8") as f:
+            content = f.read()
+
+        if len(content) > MAX_CHARS:
+            return (
+                content[:MAX_CHARS]
+                + f'\n[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+            )
+
+        return content
+
+    except Exception as e:
+        return f"Error reading file: {e}"
---
+++ ai-agents/test_get_file_content.py
@@ -0,0 +1,27 @@
+from functions.get_file_content import get_file_content 
+
+def test():
+  result = get_file_content("calculator", "main.py")
+  print("Result for main.py file:")
+  print(result)
+  print("")
+
+  result = get_file_content("calculator", "pkg/calculator.py")
+  print("Result for pkg/calculator.py directory:")
+  print(result)
+  print("")
+
+
+  result = get_file_content("calculator", "/bin/cat")
+  print('Result for "/bin/cat" directory:')
+  print(result)
+  print("")
+
+
+  result = get_file_content("calculator", "pkg/does_not_exist.py")
+  print('Result for "pkg/does_not_exist.py" directory:')
+  print(result)
+  print("")
+
+
+test()
