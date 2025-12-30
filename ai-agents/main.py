c221e1698ae9c387e7516a541218c6d427b8353c
code735 <prathmeshjkumbhar7777777@gmail.com>
2025-12-30 19:51:49 +0530

    functions declarations

--- ai-agents/main.py
+++ ai-agents/main.py
@@ -3,6 +3,8 @@ import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from prompts import system_prompt
+from call_function import available_functions
 
 def main():
     load_dotenv()
@@ -20,7 +22,12 @@ def main():
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=messages
+        model="gemini-2.5-flash", 
+        contents=messages,
+        config = types.GenerateContentConfig(
+          tools=[available_functions],
+          system_instruction = system_prompt
+        )
     )
 
     if response is None or response.usage_metadata is None:
@@ -33,5 +40,8 @@ def main():
       print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     else:
       print(response.text)
+
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 main()
